7ed18b2ecd56a1549bf380bc08e105f5443c7ed6
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2024-07-05 14:29:34 -0300

    adding AbGtupoOpcoes

--- src/index.tsx
+++ src/index.tsx
@@ -17,3 +17,4 @@ export const Thing: FC<Props> = ({ children }) => {
 export * from './componentes/AbBotao';
 export * from './componentes/AbCard';
 export * from './componentes/AbTag';
+export * from './componentes/AbGrupoOpcoes';
